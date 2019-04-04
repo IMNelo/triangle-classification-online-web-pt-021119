1e5cc71346770245b2e9dfac9814d9f97a52773d
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-04-04 08:09:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
 
   attr_accessor :equilateral, :isosceles, :scalene
 
-  def initialize(equilateral:, isosceles:, scalene:)
+  def initialize(attributes)
     @equilateral = equilateral
     @isosceles = isosceles
     @scalene = scalene
